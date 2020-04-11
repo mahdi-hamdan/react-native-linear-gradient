a51b00f3420165e286c0110c51cf48962a837116
Mahdi Hamdan <mahdi.hamdan@mbc.net>
2020-04-11 22:36:55 +0300

    initial commit

---
+++ common.js
@@ -0,0 +1,22 @@
+// @flow
+
+import type { ElementProps } from 'react';
+import { requireNativeComponent, View } from 'react-native';
+
+export default requireNativeComponent('BVLinearGradient');
+
+export type Point = $Exact<{x: number, y: number}>;
+type LinearGradientProps = {
+  start?: Point;
+  end?: Point;
+  colors: string[];
+  locations?: number[];
+  useAngle?: boolean;
+  angleCenter?: Point;
+  angle?: number;
+  strokeWidth?: number;
+  transparent?: boolean;
+};
+
+type ViewProps = ElementProps<typeof View>;
+export type Props = {| ...$Exact<LinearGradientProps>, ...ViewProps |}
---
+++ index.d.ts
@@ -0,0 +1,20 @@
+declare module 'react-native-linear-gradient' {
+  import * as React from 'react';
+  import * as ReactNative from 'react-native';
+
+  export interface LinearGradientProps extends ReactNative.ViewProps {
+    colors: (string | number)[];
+    start?: { x: number; y: number };
+    end?: { x: number; y: number };
+    locations?: number[];
+    useAngle?: boolean;
+    angleCenter?: {x: number, y: number};
+    angle?: number;
+    strokeWidth?: number;
+    transparent?: boolean;
+  }
+
+  export class LinearGradient extends React.Component<LinearGradientProps> {}
+
+  export default LinearGradient;
+}
